f7b0db709229b88bea864b62108dea4c343345e0
DevSottie <j.vitorsilva.dev@gmail.com>
2021-01-19 05:41:15 -0300

    bugs de duplo clique removidos

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ let lockBoard = false;
 
 function flipCard(){
     if(lockBoard) return;
-    
+    if(this == firstCard) return;
+
     this.classList.add('flip');
 
     if(!hasFlippedCard){
@@ -34,11 +35,17 @@ function disableCards(){
 }
 
 function unflipCards(){
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        lockBoard = false;
     }, 1500);
 }
+
+
 
 cards.forEach((card)=>{
     card.addEventListener('click', flipCard);
